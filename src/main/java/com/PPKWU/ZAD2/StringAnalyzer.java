59d344cb57d1ff8d5080bb2b4eff316693f8c19f
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-11-03 15:01:30 +0100

    Annotation for parameter added

--- src/main/java/com/PPKWU/ZAD2/StringAnalyzer.java
+++ src/main/java/com/PPKWU/ZAD2/StringAnalyzer.java
@@ -1,12 +1,13 @@
 package com.PPKWU.ZAD2;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StringAnalyzer {
     @RequestMapping(path = "/analyze")
-    public String Analyze(String entryText)
+    public String Analyze(@RequestParam(value = "input", required = false)String entryText)
     {
         return "";
     }
