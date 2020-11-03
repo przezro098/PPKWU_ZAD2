9dc9258699b99bdcb11794caa52f8ab076a107a8
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-11-03 15:22:12 +0100

    Algorythm for counting special chars added

--- src/main/java/com/PPKWU/ZAD2/StringAnalyzer.java
+++ src/main/java/com/PPKWU/ZAD2/StringAnalyzer.java
@@ -14,12 +14,13 @@ public class StringAnalyzer {
 
             if (Character.isUpperCase(entryText.charAt(i)))
                 upperCase++;
-
             if (Character.isLowerCase(entryText.charAt(i)))
                 lowerCase++;
             if (Character.isDigit(entryText.charAt(i)))
                 digits++;
-            }
+            if(!Character.isLetterOrDigit(entryText.charAt(i)))
+                specialChar++;
+        }
         return "Lowercase letters : " + String.valueOf(lowerCase)
                + ", uppercase letters : " + String.valueOf(upperCase)
                + ", digits : " + String.valueOf(digits)
