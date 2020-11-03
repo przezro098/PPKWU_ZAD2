b7700a93e544a1136984ddcdec7d34a1c7ddef69
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-11-03 14:58:40 +0100

    Analyze method & annotations added

--- src/main/java/com/PPKWU/ZAD2/StringAnalyzer.java
+++ src/main/java/com/PPKWU/ZAD2/StringAnalyzer.java
@@ -1,5 +1,14 @@
 package com.PPKWU.ZAD2;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StringAnalyzer {
+    @RequestMapping(path = "/analyze")
+    public String Analyze(String entryText)
+    {
+        return "";
+    }
 
 }
